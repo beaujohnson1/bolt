076531ba7e075ae0dbee62b2d49bee2d4ebad897
beaujohnson1 <officialbeaujohnson@gmail.com>
2025-07-30 11:07:13 -0700

    Remove testimonials section for authenticity

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,6 @@ import PricingCalculator from './components/PricingCalculator';
 import SolutionSection from './components/SolutionSection';
 import HowItWorks from './components/HowItWorks';
 import Features from './components/Features';
-import Testimonials from './components/Testimonials';
 import Pricing from './components/Pricing';
 import FAQ from './components/FAQ';
 import FinalCTA from './components/FinalCTA';
@@ -24,7 +23,6 @@ function App() {
       <SolutionSection />
       <HowItWorks />
       <Features />
-      <Testimonials />
       <Pricing />
       <FAQ />
       <FinalCTA />
